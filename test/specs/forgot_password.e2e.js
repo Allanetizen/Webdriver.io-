0a6ff0321635830d868388526dd399b0c4d9ee7d
Allan <allankosgei747@gmail.com>
2022-03-03 11:54:27 +0300

    forgot password page

---
+++ test/specs/forgot_password.e2e.js
@@ -0,0 +1,19 @@
+
+describe("forgot password page",function(){
+    it("should retrieve password", function(){
+        
+       browser.getUrl('/forgot_password')
+       //header
+       const header = $('h1=Internal Server Error')
+        //retrieve button
+       const btnRetrieve =$("#form_submit")
+       //setValue
+       btnRetrieve.setValue('email@gmail.com')
+
+       //assertion
+       expect(header).toHaveTextContaining('Internal Server Error')
+
+
+    })
+
+});
