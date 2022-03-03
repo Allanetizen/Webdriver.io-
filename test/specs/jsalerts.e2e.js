7515f56979991760f21ddde6501779b5853f4f86
Allan <allankosgei747@gmail.com>
2022-03-03 10:50:00 +0300

    modified jsalerts

--- test/specs/jsalerts.e2e.js
+++ test/specs/jsalerts.e2e.js
@@ -1,32 +1,41 @@
+
+
 describe("Javascript alerts", () => {
-    it("should test alert", () => {
-        browser.url('/javascript_alerts')
-        const btnAlert = $('button=Click for JS Alert');
-        
-        //click button 
-        btnAlert.click();
-        //Assertion
-       expect(btnAlert).toBeClickable()
-        
-      });
-      it("should test confirm", () => {
-        browser.url('/javascript_alerts')
-        const btnConfirm = $('button=Click for JS Confirm');
-        
-        //click button 
-        btnConfirm.click();
-        //Assertion
-       expect(btnConfirm).toBeClickable()
-        
-      });
-      it("should test prompt", () => {
-        browser.url('/javascript_alerts')
-        const btnPrompt = $('button=Click for JS Prompt');
-        
-        //click button 
-        btnPrompt.click();
-        //Assertion
-       expect(btnPrompt).toBeClickable()
-        
-      });
+  it("should test alert", () => {
+    browser.url("/javascript_alerts");
+    const btnAlert = $("button=Click for JS Alert");
+    //result
+    const resultText = $("#result");
+
+    //click button
+    btnAlert.click();
+    
+    //Assertion
+    expect(btnAlert).toBeClickable();
+    expect(resultText).toHaveTextContaining(
+      "You successfully clicked an alert"
+    );
+  });
+  it("should test confirm", () => {
+    browser.url("/javascript_alerts");
+    const btnConfirm = $("button=Click for JS Confirm");
+    //result
+    const resultText = $("#result");
+
+    //click button
+    btnConfirm.click();
+    //Assertion
+    expect(btnConfirm).toBeClickable();
+  });
+  it("should test prompt", () => {
+    browser.url("/javascript_alerts");
+    const btnPrompt = $("button=Click for JS Prompt");
+    //result
+    const resultText = $("#result");
+
+    //click button
+    btnPrompt.click();
+    //Assertion
+    expect(btnPrompt).toBeClickable();
+  });
 });
