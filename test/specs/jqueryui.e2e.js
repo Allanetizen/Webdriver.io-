b45cdd9646dad20cd03b3b749f9476a248bf4110
Allan <allankosgei747@gmail.com>
2022-03-03 13:47:57 +0300

    hover for jquery UI

--- test/specs/jqueryui.e2e.js
+++ test/specs/jqueryui.e2e.js
@@ -2,6 +2,7 @@
 
 describe("JQuery Ui page",function(){
     it("should click on disabled link", function(){
+        browser.getUrl('/jqueryui/menu')
         //disabled link
         const disabledLink =$('a=Disabled')
         //click
@@ -11,14 +12,21 @@ describe("JQuery Ui page",function(){
 
     })
     it("should click on enabled link", function(){
+
+        browser.getUrl('/jqueryui/menu')
         //enabled link
         const enabledLink =$('a=Enabled')
+        //link hovered to
+        const pdfLink =$('a=PDF')
+        
+        browser.elementHover(pdfLink)
         //click
         enabledLink.click()
         //assertions
         expect(enabledLink).toBeClickable()
         expect(enabledLink).toHaveTextContaining(
             "Enabled");
+
 
     })
 })
