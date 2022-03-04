c415bc502c888558a8461a8c218cef1b6d0c6cde
Allan <allankosgei747@gmail.com>
2022-03-04 13:12:15 +0300

    dropdown

--- test/specs/dropdown.e2e.js
+++ test/specs/dropdown.e2e.js
@@ -2,24 +2,32 @@
 
 
 describe('show the drop down items', function () {
-    it("show the dropdown list text", () => {
+    it("show the dropdown option 2", () => {
 
       browser.url('/dropdown')
         
         //dropDown.
         const drp = $('#dropdown');
+        const value = drp.getValue()
 
         //click the dropdown
         const list = $$("select option");
        list.forEach((element) => {
            element.click();
        });
-        
 
         //select using index
+                
         optOne=drp.selectByIndex(1)
 
         optTwo=drp.selectByIndex(2)
+
+        drp.click()
+
+        expect(value).toHaveValue('Option 2')
+
+
+        
       //assertions
         
         expect(optOne).toHaveValueContaining(1)
@@ -27,8 +35,7 @@ describe('show the drop down items', function () {
 
         expect(optOne).toHaveTextContaining("Option 1")
         expect(optTwo).toHaveTextContaining("Option 2")
-
-
+       
 
       });
     
